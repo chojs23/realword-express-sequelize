8c3e263b4829fde35f8b06029103bd20412d02b9
neo <chojs990222@gmail.com>
2022-12-04 17:36:07 +0900

    fix auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const { verify } = require("../util/jwt");
 const User = require("../models/User");
+const ErrorResponse = require("../util/errorResponse");
 
 exports.protect = async (req, res, next) => {
   let token;
@@ -18,7 +19,12 @@ exports.protect = async (req, res, next) => {
   try {
     const decoded = await verify(token);
 
-    req.user = await User.findByPk(decoded.id);
+    const user = await User.findByPk(decoded.id);
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
     next();
   } catch (error) {
     return next(new ErrorResponse("Not authorized to access this route", 401));
